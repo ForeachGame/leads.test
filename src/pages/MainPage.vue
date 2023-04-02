b89f895453759e05a6309243928554c834c38d0b
Aleksandr Cheremisinov <alexcheremisinov@gmail.com>
2023-04-02 23:50:30 +0500

    page create

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import Vue from 'vue'
+import { WNavbar, WCategories, WPagination } from '@/widgets'
+
+export default Vue.extend({
+    name: 'MainPage',
+    components: { WNavbar, WCategories, WPagination }
+})
+</script>
+
+<template>
+    <div class="MainPage">
+        <w-navbar />
+        <w-categories />
+        <w-pagination />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
