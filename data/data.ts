bf492569ef1304be1ecab8ee93df0727f2fdc8a9
ForeachGame <alexcheremisinov@gmail.com>
2023-04-04 04:05:08 +0500

    build fix

--- data/data.ts
+++ data/data.ts
@@ -5,7 +5,7 @@ for (let i = 1; i <= 100; i++) {
     dataArticles.push({
         id: i,
         title: `Статья ${i}`,
-        image: '/assets/images/default-image.jpg',
+        image: './assets/images/default-image.jpg',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         likes: Math.round(Math.random() * 50)
     })
